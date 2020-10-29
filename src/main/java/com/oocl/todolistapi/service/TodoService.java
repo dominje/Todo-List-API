d9d3b418d813af6e601660c4b1e55ad73d3348a2
dominje <jericah.domingo@oocl.com>
2020-10-29 14:45:13 +0800

    [Todo-List-API][Andrey&Jericah] feature: implement add task to database

--- src/main/java/com/oocl/todolistapi/service/TodoService.java
+++ src/main/java/com/oocl/todolistapi/service/TodoService.java
@@ -17,4 +17,8 @@ public class TodoService {
     public List<Todo> getAll() {
         return todoRepository.findAll();
     }
+
+    public Todo addTodoList(Todo todo) {
+        return todoRepository.save(todo);
+    }
 }
